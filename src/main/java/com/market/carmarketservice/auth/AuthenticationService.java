9d3a250892bb1961c0b2314a8ab06c1d57959c2c
Admin <19130112@st.hcmuaf.edu.vn>
2023-03-30 14:56:15 +0700

    spring security

---
+++ src/main/java/com/market/carmarketservice/auth/AuthenticationService.java
@@ -0,0 +1,62 @@
+package com.market.carmarketservice.auth;
+
+import com.market.carmarketservice.config.JwtService;
+import com.market.carmarketservice.user.Role;
+import com.market.carmarketservice.user.User;
+import com.market.carmarketservice.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final UserRepository repository;
+    private final JwtService jwtService;
+
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .address(request.getAddress())
+                .phone(request.getPhone())
+                .avatar(request.getAvatar())
+                .birthyear(request.getBirthyear())
+                .username(request.getUsername())
+                .password(passwordEncoder().encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse
+                .builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getUsername(),
+                        request.getPassword()
+                )
+        );
+        var user = repository.findByUsername(request.getUsername())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        return AuthenticationResponse
+                .builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
