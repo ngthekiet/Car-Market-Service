26db4bf9d363e1195c2c9529efb1de4c4427c723
Admin <19130112@st.hcmuaf.edu.vn>
2023-05-07 23:51:12 +0700

    update model

--- src/main/java/com/market/carmarketservice/model/info/InfoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.market.carmarketservice.model.info;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InfoRepository extends JpaRepository<Info, Integer> {
-    boolean existsInfoById(int id);
-}
